747156e008b41d06adb128345272076c680d278d
bluebirz <9207891+bluebirz@users.noreply.github.com>
2025-09-30 19:38:47 +0200

    add airflow3

---
+++ airflow3/dags/sample_project/tests/dag_integrity.py
@@ -0,0 +1,26 @@
+import unittest
+from airflow.models import DagBag
+
+
+class TestDags(unittest.TestCase):
+    """DAG Test Case"""
+
+    LOAD_THRESHOLD_SECONDS = 2
+
+    def setUp(self):
+        self.dagbag = DagBag()
+
+    def test_dags_syntax(self):
+        """Assert DAG bag load correctly"""
+        for key in self.dagbag.dags:
+            print(key)
+        self.assertTrue(len(self.dagbag.dags) > 0, "need at least 1 dag")
+        self.assertFalse(
+            len(self.dagbag.import_errors),
+            f"DAG import errors. Errors: {self.dagbag.import_errors}",
+        )
+
+
+if __name__ == "__main__":
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(TestDags)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
